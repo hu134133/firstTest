246fb05f17ea459bd74f225fb7c93802f2d0c993
huhao <416302784@qq.com>
2019-03-23 14:05:39 +0800

    搭建Python框架

---
+++ TestGitGithub/TestGitGithub/TestGitGithub/main.py
@@ -0,0 +1,18 @@
+# coding=UTF-8
+from scrapy import cmdline
+
+#print('--------------->hello scrapy')
+#cmdline.execute("scrapy crawl books -o books.csv".split())
+#cmdline.execute("scrapy crawl login".split())
+#cmdline.execute("scrapy crawl books1 -o books1.csv".split())
+
+#cmdline.execute("scrapy crawl jincai -o football.csv".split())
+
+cmdline.execute("scrapy crawl jincai".split())
+
+#cmdline.execute("scrapy crawl bisai".split())
+#cmdline.execute("scrapy crawl bisai -o bisai1.csv".split())
+#cmdline.execute("scrapy crawl bisai -o bisai2.csv".split())
+
+
+
